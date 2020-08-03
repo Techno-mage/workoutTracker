9b00289ce35cf4e35ccd3dab4b0969ba171c44f7
Erik Schaal <schaa077@umn.edu>
2020-08-03 18:32:30 -0500

    We are like, 99.9% the way there

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -15,7 +15,7 @@ module.exports = function(app) {
         console.log(req.body)
         //var input = JSON.parse(req.body.exercize);
         //console.log(input)
-        db.Workout.findOneAndUpdate({_id: req.params.id}, {$push: {exercises: req.body.exercize}}, {new: true})
+        db.Workout.findOneAndUpdate({_id: req.params.id}, {$push: {exercises: req.body}}, {new: true})
         .then(dbNote => {
          res.json(dbNote);
         })
@@ -26,6 +26,16 @@ module.exports = function(app) {
 
     app.post("/api/workouts", (req, res) => {
         db.Workout.create({})
+        .then(dbNote => {
+         res.json(dbNote);
+        })
+        .catch(err => {
+        res.json(err);
+        });
+    })
+
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.find({})
         .then(dbNote => {
          res.json(dbNote);
         })
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 // Start the server
